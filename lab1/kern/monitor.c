02f01d3d42e5f6f54e4d094419e1dfbd9cda344b
Jianan Ding <ktnbfw@gmail.com>
2020-05-25 16:58:32 -0400

    After exercise 8.4

--- lab1/kern/monitor.c
+++ lab1/kern/monitor.c
@@ -114,7 +114,9 @@ monitor(struct Trapframe *tf)
 
 	cprintf("Welcome to the JOS kernel monitor!\n");
 	cprintf("Type 'help' for a list of commands.\n");
-
+	//Exercise 8.4
+	//unsigned int i = 0x00646c72;
+    //cprintf("H%x Wo%s", 57616, &i);
 
 	while (1) {
 		buf = readline("K> ");
